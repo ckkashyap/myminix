e74d07f97a089dd40db45e8a05b13b59d0a634ec
ckkashyap <ck_kashyap@yahoo.com>
2009-01-07 16:05:30 +0530

    kprintf for debugging

---
+++ kernel/kprintf.c
@@ -0,0 +1,139 @@
+/* Some screen stuff.  */
+/* The number of columns.  */
+#define COLUMNS                 80
+/* The number of lines.  */
+#define LINES                   24
+/* The attribute of an character.  */
+#define ATTRIBUTE               0x1f
+/* The video memory address.  */
+#define VIDEO                   0xB8000
+
+/* Variables.  */
+/* Save the X position.  */
+static int xpos;
+/* Save the Y position.  */
+static int ypos;
+/* Point to the video memory.  */
+static volatile unsigned char *video=(unsigned char *) VIDEO;;
+
+
+static void
+itoa (char *buf, int base, int d)
+{
+  char *p = buf;
+  char *p1, *p2;
+  unsigned long ud = d;
+  int divisor = 10;
+
+  /* If %d is specified and D is minus, put `-' in the head.  */
+  if (base == 'd' && d < 0)
+    {
+      *p++ = '-';
+      buf++;
+      ud = -d;
+    }
+  else if (base == 'x')
+    divisor = 16;
+
+  /* Divide UD by DIVISOR until UD == 0.  */
+  do
+    {
+      int remainder = ud % divisor;
+
+      *p++ = (remainder < 10) ? remainder + '0' : remainder + 'a' - 10;
+    }
+  while (ud /= divisor);
+
+  /* Terminate BUF.  */
+  *p = 0;
+
+  /* Reverse BUF.  */
+  p1 = buf;
+  p2 = p - 1;
+  while (p1 < p2)
+    {
+      char tmp = *p1;
+      *p1 = *p2;
+      *p2 = tmp;
+      p1++;
+      p2--;
+    }
+}
+
+
+
+static void scroll(){
+	int r;
+	int *dword=(int *)0xb8000;
+	for (r=0;r<40*LINES;r++){
+		dword[r]=dword[r+40];
+	}
+}
+static void
+putchar (int c)
+{
+  if (c == '\n' || c == '\r')
+    {
+    newline:
+      xpos = 0;
+      ypos++;
+      if (ypos >= LINES){
+        ypos--;
+	scroll();
+      }
+      return;
+    }
+
+  *(video + (xpos + ypos * COLUMNS) * 2) = c & 0xFF;
+  *(video + (xpos + ypos * COLUMNS) * 2 + 1) = ATTRIBUTE;
+
+  xpos++;
+  if (xpos >= COLUMNS)
+    goto newline;
+}
+
+void
+kprintf (const char *format, ...)
+{
+  char **arg = (char **) &format;
+  int c;
+  char buf[20];
+
+  arg++;
+
+  while ((c = *format++) != 0)
+    {
+      if (c != '%')
+        putchar (c);
+      else
+        {
+          char *p;
+
+          c = *format++;
+          switch (c)
+            {
+            case 'd':
+            case 'u':
+            case 'x':
+              itoa (buf, c, *((int *) arg++));
+              p = buf;
+              goto string;
+              break;
+
+            case 's':
+              p = *arg++;
+              if (! p)
+                p = "(null)";
+
+            string:
+              while (*p)
+                putchar (*p++);
+              break;
+
+            default:
+              putchar (*((int *) arg++));
+              break;
+            }
+        }
+    }
+}
